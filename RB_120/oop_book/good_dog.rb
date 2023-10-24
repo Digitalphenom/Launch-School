55e7955ae1e890c982e9f0c8018386c41ab1782b
digitalphenom <digitalphenom0@gmail.com>
2023-10-24 15:04:50 -0700

    added functionality to GoodDog class

--- RB_120/oop_book/good_dog.rb
+++ RB_120/oop_book/good_dog.rb
@@ -1,6 +1,36 @@
 class GoodDog
+  attr_accessor :name, :gender, :weight
+
+  def initialize(name, gender, weight)
+    @name = name
+    @gender = gender
+    @weight = weight
+  end
+
+  def speak
+    "#{name} says Boooorff!"
+  end
+
+  def change_info(n, g, w)
+    self.name = n
+    self.gender = g 
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is a #{self.gender}"
+  end
 end
 
-sparky = GoodDog.new
+pepper = GoodDog.new("Pepper", "Male", "25 lbs")
+tina = GoodDog.new("Tina", "Female", "18 lbs")
 
-p sparky
+p pepper.speak
+p tina.name
+
+p pepper.name
+p pepper.info
+
+tina.change_info("Cherry", "Female", "12 lbs")
+puts
+p tina.info
