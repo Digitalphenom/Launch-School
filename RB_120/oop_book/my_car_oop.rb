5ff9de0f01d3cb2dae882e6a9d5f800c7ef11465
digitalphenom <digitalphenom0@gmail.com>
2023-10-24 20:04:03 -0700

    renamed file

---
+++ RB_120/oop_book/my_car_oop.rb
@@ -0,0 +1,57 @@
+=begin
+Create a class called MyCar. When you initialize a new instance or object of the class, allow the user to define some instance variables that tell us the year, color, and model of the car. Create an instance variable that is set to 0 during instantiation of the object to track the current speed of the car as well. Create instance methods that allow the car to speed up, brake, and shut the car off.
+
+⋄≂≂▫≂≂▫≂≂▫≂▫≂▫≂≂▫≂≂▫≂⋄—◟ 𝓟roblem ◞—⋄≂▫≂≂▫≂≂▫≂≂▫≂▫≂≂▫≂≂▫≂≂≂⋄
+
+▣ Create the class MyCar
+▣ Allow user to define an instance variable
+  □ Year, color, model
+▣ Create an instance variable set to 0 that tracks current speed
+▣ Create instance method that defines the following behavior:
+  □ speed up, brake, shut off car
+
+Add a class method to your MyCar class that calculates the gas mileage (i.e. miles per gallon) of any car.
+
+=end
+
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+
+  def self.calculate_gas_milage(miles, gallons)
+    puts "Your miles per gallons is: #{miles / gallons} mpg"
+  end
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+
+  def speed_up(x)
+    @speed += x
+  end
+
+  def change_color(input)
+    self.color = input
+  end
+
+  def brake(x)
+    @speed -= x
+  end
+
+  def current_speed
+    puts "Your current speed is #{@speed} mph"
+  end
+
+  def shut_off(input)
+    return "Vehicle is off" if input.include?("off") && @speed.zero?
+    "Please turn off the vehicle"
+  end
+
+end
+
+toyota = MyCar.new(2009, "Red", "Yaris")
+
+MyCar.calculate_gas_milage(235, 10)
