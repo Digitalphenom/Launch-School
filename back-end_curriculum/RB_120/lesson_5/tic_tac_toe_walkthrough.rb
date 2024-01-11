f6e130b96799d3671527ccbc1d487f6e9ee87e4a
digitalphenom <digitalphenom0@gmail.com>
2024-01-10 16:03:37 -0800

    rubocop refactor

--- back-end_curriculum/RB_120/lesson_5/tic_tac_toe_walkthrough.rb
+++ back-end_curriculum/RB_120/lesson_5/tic_tac_toe_walkthrough.rb
@@ -1,50 +1,36 @@
-=begin
- Tic Tac Toe is a 2-player board game played on a 3x3 grid. Players take turns in marking a square. The first player to mark 3 squares in a row wins
-
- Nouns: board, player, square, grid
- Verbs: play, mark
-
-▣ Board
-▣ Square
-▣ Player
-  □ mark
-  □ play
-
-=end
-
-# It is beest to access a method instead of directly accessing instance variables so we would access the method directly.
-
-require "pry"
-
 class Board
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
-                [[1, 5, 9], [3, 5, 7]]
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # colums
+                  [[1, 5, 9], [3, 5, 7]]
 
   def initialize
     @squares = {}
-    (1..9).each { |key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def draw_board
-    puts "         |       |"
+    puts '         |       |'
     puts "      #{@squares[1]}  |   #{@squares[2]}   |  #{@squares[3]}"
-    puts "         |       |"
-    puts "   ------+-------+------"
-    puts "         |       |"
+    puts '         |       |'
+    puts '   ------+-------+------'
+    puts '         |       |'
     puts "      #{@squares[4]}  |   #{@squares[5]}   |  #{@squares[6]}"
-    puts "         |       |"
-    puts "   ------+-------+------"
-    puts "         |       |"
+    puts '         |       |'
+    puts '   ------+-------+------'
+    puts '         |       |'
     puts "      #{@squares[7]}  |   #{@squares[8]}   |  #{@squares[9]}"
-    puts "         |       |"
-    puts ""
+    puts '         |       |'
+    puts
   end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
 
   def []=(key, marker)
     @squares[key].marker = marker
   end
-  
+
   def unmarked_keys
     @squares.select { |_, sq| sq.unmarked? }.keys
   end
@@ -58,38 +44,35 @@ class Board
   end
 
   def reset
-    (1..9).each { |key| @squares[key] = Square.new}
+    (1..9).each { |key| @squares[key] = Square.new }
   end
 
   # returns winning marker or nil
   def winning_marker
     WINNING_LINES.each do |line|
       squares = @squares.values_at(*line)
-      if count_markers(squares)
-        return squares.first.marker
-      end
+      return squares.first.marker if count_markers(squares)
     end
     nil
   end
 
-  private 
+  private
 
   def count_markers(squares)
-    if squares.collect(&:marker).count("X") == 3
-      squares.collect(&:marker).count("X") 
-    elsif squares.collect(&:marker).count("O") == 3
-      squares.collect(&:marker).count("O")
+    if squares.collect(&:marker).count('X') == 3
+      squares.collect(&:marker).count('X')
+    elsif squares.collect(&:marker).count('O') == 3
+      squares.collect(&:marker).count('O')
     end
   end
-
 end
 
-class Square 
-  INITIAL_MARKER = " "
+class Square
+  INITIAL_MARKER = ' '.freeze
 
   attr_accessor :marker
 
-  def initialize(marker=INITIAL_MARKER)
+  def initialize(marker = INITIAL_MARKER)
     @marker = marker
   end
 
@@ -100,7 +83,6 @@ class Square
   def unmarked?
     marker == INITIAL_MARKER
   end
-
 end
 
 class Player
@@ -112,91 +94,108 @@ class Player
 end
 
 class TTTGame
-  HUMAN_MARKER = "X"
-  COMPUTER_MARKER = "O"
+  HUMAN_MARKER = 'X'.freeze
+  COMPUTER_MARKER = 'O'.freeze
 
   attr_reader :board, :human, :computer
-  
+
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
-    @turns = (1..9).to_a.map { |num| num.odd? ? true : false}
+    @turns = (1..9).to_a.map { |num| num.odd? ? true : false }
   end
 
   def play
     clear
-    display_welcome_message
-    display_who_moves_first
-    move_choice
-    display_board
-
-    loop do
-      loop do 
-        current_player_moves
-        break if board.someone_won? || board.full?
-        clear_screen_and_display_board
-      end
-      display_result
-      break unless play_again?
-      reset
-      display_play_again_message
-      display_who_moves_first
-      clear_screen_and_display_board
-
-    end
+    main_game
     display_goodbye_message
   end
 
   private
 
-  def toggle_turn(choice)
+  def main_game
+    display_welcome_message
+    display_who_moves_first_and_player_option
+    display_board
+
+    loop do
+      player_move
+      break unless play_again?
+
+      reset
+      display_play_again_message
+      display_who_moves_first_and_player_option
+    end
+  end
+
+  def player_move
+    loop do
+      current_player_moves
+      break if board.someone_won? || board.full?
+
+      clear_screen_and_display_board
+    end
+    display_result
+  end
+
+  def display_who_moves_first
+    puts '      Who goes first?'
+    puts 'Press: 1 for Human | 2 for Computer'
+  end
+
+  def display_who_moves_first_and_player_option
+    display_who_moves_first
+    who_moves_first_choice
+  end
+
+  def who_moves_first_choice
+    choice = gets.chomp
+    whos_turn(choice)
+  end
+
+  def whos_turn(choice)
+    choice == '2' ? toggle_turn : @turns
+  end
+
+  def toggle_turn
     @turns.shift
     @turns << false
   end
 
-  def whos_turn(choice)
-    choice == "2" ? shift_turn(choice) : @turns
-  end
-
-  def display_who_moves_first
-    puts "      Who goes first?"
-    puts "Press: 1 for Human | 2 for Computer"
-  end
-
-  def move_choice
-    choice = gets.chomp
-    toggle_turn(choice)
+  def current_player_moves
+    turns.shift ? human_moves : computer_moves
   end
 
   def reset_turns
-    @turns = (1..9).to_a.map { |num| num.odd? ? true : false}
+    @turns = (1..9).to_a.map { |num| num.odd? ? true : false }
   end
 
   def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
+    puts 'Welcome to Tic Tac Toe!'
   end
 
   def display_goodbye_message
-    puts "Thanks for playing!"
+    puts 'Thanks for playing!'
   end
 
   def clear
-    system "clear"
+    system 'clear'
   end
 
   def clear_screen_and_display_board
-    clear()
-    display_board()
+    clear
+    display_board
   end
 
   def human_moves
-    puts "Chose a square between #{board.unmarked_keys.join(", ")}:"
+    puts "Chose a square between #{board.unmarked_keys.join(', ')}:"
     square = nil
-    loop do 
+    loop do
       square = gets.chomp.to_i
       break if board.unmarked_keys.include?(square)
-      puts "Sorry, that's not a valid choice."
+
+      puts 'Sorry, that\'s not a valid choice.'
     end
     board[square] = human.marker
   end
@@ -210,23 +209,24 @@ class TTTGame
 
     case board.winning_marker
     when human.marker
-      puts "You won!"
+      puts 'You won!'
     when computer.marker
-      puts "Computer won!"
+      puts 'Computer won!'
     else
-      puts "Its a tie!"
+      puts 'Its a tie!'
     end
   end
 
   def play_again?
-    answer = ""
+    answer = ''
     loop do
-      puts "Would you like to play again? (y/n)"
+      puts 'Would you like to play again? (y/n)'
       answer = gets.chomp.downcase
-      break if %w(y n).include?(answer)
-      puts "Sorry, must be y or n"
+      break if %w[y n].include?(answer)
+
+      puts 'Sorry, must be y or n'
     end
-    answer == "y"
+    answer == 'y'
   end
 
   def display_board
@@ -243,24 +243,12 @@ class TTTGame
   end
 
   def display_play_again_message
-    puts "Let's play again"
+    puts 'Let\'s play again'
     puts
   end
 
-  def current_player_moves
-    turns.shift ? human_moves : computer_moves
-  end
-
   attr_accessor :turns
-
 end
 
 # Start Game
 TTTGame.new.play
-
-
-
-
-
-
-
