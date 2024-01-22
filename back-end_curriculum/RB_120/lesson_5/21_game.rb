61890200bb7e4d65c3ff3c53ababa288a60db186
digitalphenom <digitalphenom0@gmail.com>
2024-01-21 21:29:19 -0800

    added get_total logic

--- back-end_curriculum/RB_120/lesson_5/21_game.rb
+++ back-end_curriculum/RB_120/lesson_5/21_game.rb
@@ -1,12 +1,12 @@
 require "yaml"
-
+require "pry"
 
 PROMPT = YAML.load_file('21_prompts.yml')
 
 class Player
   def initialize
-    @user# what would the "data" or "states" of a Player object entail?
-    @dealer# maybe cards? a name?
+    # what would the "data" or "states" of a Player object entail?
+    # maybe cards? a name?
   end
 
   def hit
@@ -28,7 +28,7 @@ class Participant
 end
 
 class Deck
-  attr_accessor :user_cards, :dealer_cards
+  attr_accessor :user_cards, :dealer_cards, :user_total, :dealer_total
 
   SUITS = [:Diamonds, :Hearts, :Spades, :Clubs]
   VALUES = [:Queen, :Jack, :King, :Ace]
@@ -37,6 +37,8 @@ class Deck
     @deck = SUITS.map { |suit| [suit, [(2..10).to_a ,VALUES].flatten] }.to_h
     @user_cards = []
     @dealer_cards = []
+    @user_card_state = nil
+    @dealer_card_state = nil
   end
 
   def deal
@@ -47,16 +49,45 @@ class Deck
   end
 
   def display_cards
-    puts "Player Cards: #{user_cards} Dealer Cards: #{dealer_cards}"
-    #"Player Cards:[jack, 10] Dealer Cards: [queen, ace]"
+    get_card_states
+    get_total
+    puts "Player Cards: #{user_cards} Total: #{@user_total} Dealer Cards: #{dealer_cards} Total: #{@dealer_total}" 
   end
+
+  def get_card_states
+    @user_card_state = Card.new(user_cards)
+    @dealer_card_state = Card.new(dealer_cards)
+  end
+
+  def get_total
+    @user_total = @user_card_state.get_total.sum
+    @dealer_total = @dealer_card_state.get_total.sum
+  end
+  
 
 end
 
 class Card
-  def initialize
+  attr_reader :player_cards
+
+  def initialize(player_cards)
+    @player_cards = player_cards
     # what are the "states" of a card?
   end
+
+  def get_total
+    player_cards.map do |card|
+      case card
+      when :Queen then 10
+      when :Jack then 10
+      when :King then 10
+      when :Ace then 11
+      else
+        card
+      end
+    end
+  end
+
 end
 
 class Game
@@ -94,7 +125,6 @@ class Game
       next unless n == "1"
       deck.deal
       show_initial_cards
-
 
       # player_turn
       # dealer_turn
