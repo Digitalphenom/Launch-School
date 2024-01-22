1bd8450f4ee08ab9b43ac718d0bd78dc0fa10bd2
digitalphenom <digitalphenom0@gmail.com>
2024-01-21 19:27:57 -0800

    set card deck logic & initial prompt messages

--- back-end_curriculum/RB_120/lesson_5/21_game.rb
+++ back-end_curriculum/RB_120/lesson_5/21_game.rb
@@ -1,7 +1,12 @@
+require "yaml"
+
+
+PROMPT = YAML.load_file('21_prompts.yml')
+
 class Player
   def initialize
-    # what would the "data" or "states" of a Player object entail?
-    # maybe cards? a name?
+    @user# what would the "data" or "states" of a Player object entail?
+    @dealer# maybe cards? a name?
   end
 
   def hit
@@ -18,41 +23,34 @@ class Player
   end
 end
 
-class Dealer
-  def initialize
-    # seems like very similar to Player... do we even need this?
-  end
-
-  def deal
-    # does the dealer or the deck deal?
-  end
-
-  def hit
-  end
-
-  def stay
-  end
-
-  def busted?
-  end
-
-  def total
-  end
-end
-
 class Participant
   # what goes in here? all the redundant behaviors from Player and Dealer?
 end
 
 class Deck
+  attr_accessor :user_cards, :dealer_cards
+
+  SUITS = [:Diamonds, :Hearts, :Spades, :Clubs]
+  VALUES = [:Queen, :Jack, :King, :Ace]
+
   def initialize
-    # obviously, we need some data structure to keep track of cards
-    # array, hash, something else?
+    @deck = SUITS.map { |suit| [suit, [(2..10).to_a ,VALUES].flatten] }.to_h
+    @user_cards = []
+    @dealer_cards = []
   end
 
   def deal
-    # does the dealer or the deck deal?
+    # the action of dealing cards, entails extracting 2 sets of 2 cards from the deck to give to each player
+    2.times { user_cards << @deck[SUITS.sample].sample }
+    2.times { dealer_cards << @deck[SUITS.sample].sample }
+
   end
+
+  def display_cards
+    puts "Player Cards: #{user_cards} Dealer Cards: #{dealer_cards}"
+    #"Player Cards:[jack, 10] Dealer Cards: [queen, ace]"
+  end
+
 end
 
 class Card
@@ -62,13 +60,52 @@ class Card
 end
 
 class Game
+  attr_reader :deck
+
+  def initialize
+    @deck = Deck.new
+  end
+
+  def show_initial_cards
+    deck.display_cards
+  end
+
+  def display_welcome_message
+    puts PROMPT["welcome"]
+    puts PROMPT["name"]
+    name = get_name
+    puts "Ok #{name} heres the deal.."
+  end
+  
+  def get_name
+    gets.chomp
+  end
+  
+  def start_game
+    puts
+    puts PROMPT["start_game"]
+    gets.chomp
+  end
+
   def start
-    deal_cards
-    show_initial_cards
-    player_turn
-    dealer_turn
-    show_result
+    display_welcome_message
+    loop do 
+      n = start_game
+      next unless n == "1"
+      deck.deal
+      show_initial_cards
+
+
+      # player_turn
+      # dealer_turn
+      # show_result
+      break
+    end
   end
 end
 
 Game.new.start
+
+#Dealer        Diana
+#[?, 5]      [5, 10]
+
