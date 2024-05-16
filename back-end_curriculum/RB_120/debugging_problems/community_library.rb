da80c4c67c0b178ad10514e341eecfe67ba78276
digitalphenom <digitalphenom0@gmail.com>
2024-05-16 16:25:27 -0700

    debugging one

---
+++ back-end_curriculum/RB_120/debugging_problems/community_library.rb
@@ -0,0 +1,57 @@
+# On line 42 of our code, we intend to display information regarding the books currently checked in to our community library. Instead, an exception is raised. Determine what caused this error and fix the code so that the data is displayed as expected.
+
+class Library
+  attr_accessor :address, :phone, :books
+
+  def initialize(address, phone)
+    @address = address
+    @phone = phone
+    @books = []
+  end
+
+  def check_in(book)
+    books.push(book)
+  end
+
+  def display_books
+    books.each do |book|
+      book.display_data
+    end
+  end
+end
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+class Book
+  attr_accessor :title, :author, :isbn
+
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+  end
+
+  def display_data
+      puts "---------------"
+      puts "Title: #{title}"
+      puts "Author: #{author}"
+      puts "ISBN: #{isbn}"
+      puts "---------------"
+  end
+end
+
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+community_library = Library.new('123 Main St.', '555-232-5652')
+learn_to_program = Book.new('Learn to Program', 'Chris Pine', '978-1934356364')
+little_women = Book.new('Little Women', 'Louisa May Alcott', '978-1420951080')
+wrinkle_in_time = Book.new('A Wrinkle in Time', 'Madeleine L\'Engle', '978-0312367541')
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+# Object collaboration
+community_library.check_in(learn_to_program)
+community_library.check_in(little_women)
+community_library.check_in(wrinkle_in_time)
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+# In this code `Library` and `Book` collaborate to extend the capabilities of the library class by leveraging behavior from the `book` class. However, an error is raised because `display_data` is only avilable on instance of `book` not on Arrays. To fix this we can invoke `#each` on the array to access each book then invoke `display_data` on each instance of book.
+
+#community_library.books.each { |book| book.display_data }
+
+# In order to enforce encapsulation by preventing the `book` array from being accessed outside the class. We can define a specialized method that performs this same actionw within the class.
+community_library.display_books
