71516bf760f46a763ea5966ed5dbb15fe8d2c398
digitalphenom <digitalphenom0@gmail.com>
2024-05-22 17:22:09 -0700

    decoupled components and defined behaviors specific to eachc class

--- back-end_curriculum/RB_120/lesson_5/21_game.rb
+++ back-end_curriculum/RB_120/lesson_5/21_game.rb
@@ -2,15 +2,16 @@ require "yaml"
 require "pry"
 
 PROMPT = YAML.load_file('21_prompts.yml')
-
 #‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
 
 class Participant
-  attr_reader :choice
+  attr_reader :choice, :cards
 
   def initialize()
     #@name = name
-    @choice = nil
+    @cards = []
+    @total = 0
+    #@choice = nil
     # what would the "data" or "states" of a Player object entail?
     # maybe cards? a name?
   end
@@ -32,55 +33,36 @@ end
 #‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
 
 class Player < Participant
+  def turn?
+    @turn = true
+    false if stay
+  end
 end
 
 class Dealer < Participant
-  def deal
-  end
 end
 
 #‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
 
 class Deck
-  attr_accessor :user_cards, :dealer_cards, :user_total, :dealer_total
+  attr_reader :deck
 
-  SUITS = [:Diamonds, :Hearts, :Spades, :Clubs]
-  RANKS = [:Queen, :Jack, :King, :Ace]
+  RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
 
   def initialize
-    @deck = SUITS.map { |suit| [suit, [(2..10).to_a ,RANKS].flatten] }.to_h
-    @user_cards = []
-    @dealer_cards = []
-    @user_card_state = nil
-    @dealer_card_state = nil
+    @deck = []
+    generate_deck
   end
 
+  def generate_deck
+    52.times {|_| deck << Card.new(RANKS.sample, SUITS.sample) } 
+  end
+
+  # If we are to more the real world version of cards, we would give the ability to `deal` to the dealer. However in our program this would require the dealer to collaborate with the deck in order to `know` the deck, which creates an additional `dependency`. For this reason it might be best to leave the dealing of cards to the `Deck`.
   def deal
-    # the action of dealing cards, entails extracting 2 sets of 2 cards from the deck to give to each player
-    2.times { user_cards << @deck[SUITS.sample].sample }
-    2.times { dealer_cards << @deck[SUITS.sample].sample }
-
-  end
-
-  def display_cards
-    get_card_states
-    get_total
-    puts "Player Cards: #{user_cards} Your running total is: #{@user_total}"
-    puts "Dealer Cards: #{dealer_cards} Dealer total: #{@dealer_total}" 
-  end
-
-  def get_card_states
-    @user_card_state = Card.new(user_cards)
-    @dealer_card_state = Card.new(dealer_cards)
-  end
-
-  def get_total
-    @user_total = @user_card_state.get_total.sum
-    @dealer_total = @dealer_card_state.get_total.sum
-  end
-
-  def add_to_deck
-    user_cards << @deck[SUITS.sample].sample
+    generate_deck if deck.empty?
+    deck.pop
   end
 
 end
@@ -88,45 +70,56 @@ end
 #‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
 
 class Card
-  attr_reader :player_cards
+  attr_reader :rank, :suit
 
-  def initialize(player_cards)
-    @player_cards = player_cards
-    # what are the "states" of a card?
+  RANK = {
+    'Jack' => 10,
+    'Queen' => 10,
+    'King' => 10,
+    'Ace' => 11
+  }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 
-  def get_total
-    player_cards.map do |card|
-      case card
-      when :Queen then 10
-      when :Jack then 10
-      when :King then 10
-      when :Ace then 11
-      else
-        card
-      end
-    end
+  def <=>(other)
+    v1 = RANK.fetch(rank, rank)
+    v2 = RANK.fetch(other.rank, other.rank)
+    v1 <=> v2
   end
+
+#  def to_s
+#    "#{rank} of #{suit}"
+#  end
 
 end
 
 #‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
-
+# The game class is responsible for aspects of the game that relate to playing and display game actions.
 class Game
-  attr_reader :deck, :player
+  attr_reader :deck, :player, :dealer
 
   def initialize
     @deck = Deck.new
     @player = Player.new
-  end
-
-  def show_initial_cards
-    puts
-    deck.display_cards
+    @dealer = Dealer.new
   end
 
   def clear_screen
     system "clear"
+  end
+
+  def get_name
+    gets.chomp
+  end
+
+  def display_welcome_message
+    puts PROMPT["welcome"]
+    puts PROMPT["name"]
+    name = get_name
+    puts "Ok #{name} heres the deal.."
   end
 
   def hit_or_stay
@@ -136,29 +129,32 @@ class Game
     @choice = gets.chomp
   end
 
-  def display_welcome_message
-    puts PROMPT["welcome"]
-    puts PROMPT["name"]
-    name = get_name
-    puts "Ok #{name} heres the deal.."
+  def display_all_cards
+    puts "Player Cards:"
+    puts "#{display_cards(player)}"
+    puts "Your running total is: #{}"
+    puts "------------------------"
+    puts "Dealer Cards:"
+    puts "#{display_cards(dealer)}"
+    puts "Dealer total: #{}" 
   end
-  
-  def get_name
-    gets.chomp
+
+  def display_cards(participant)
+    participant.cards.each { |card| puts "- #{card.rank} of #{card.suit}"}
+    nil
   end
-  
+
   def start_game
     puts
     puts PROMPT["start_game"]
     gets.chomp
   end
 
-
   def player_turn
     loop do 
       hit_or_stay
       if hit_or_stay_choice == "1"
-        deck.add_to_deck
+        deck.deal
         clear_screen
         show_initial_cards
       else
@@ -166,6 +162,16 @@ class Game
       end
       break
     end
+  end
+
+  def deal_card
+    player.cards << deck.deal if player.turn?
+    dealer.cards << deck.deal
+  end
+
+  def init_cards
+    2.times { player.cards << deck.deal }
+    2.times { dealer.cards << deck.deal }
   end
     # here we prompt the user if he wants to hit or stay and fill in the required logic for each choice.
     # if stay then we pass the turn to dealer
@@ -177,8 +183,8 @@ class Game
       n = start_game
       next  unless n == "1"
       clear_screen
-      deck.deal
-      show_initial_cards
+      init_cards
+      display_all_cards
 
       player_turn()
       # after dealers turn, check cards and show results
