721f5bfb62f86dae3f9a6cc710b524785bf9a779
digitalphenom <digitalphenom0@gmail.com>
2024-09-13 17:37:52 -0700

    modified custom reduce

--- RB_130/closures/custom_reduce.rb
+++ RB_130/closures/custom_reduce.rb
@@ -1,39 +1,48 @@
-def reduce(arr, acc = 0)
-  if arr.any? { |el| el.is_a?(String)}
-    arr = arr.map { |el| el.to_s}
-    acc = ''
-  elsif arr.any? { |el| el.is_a?(Array)}
-    arr = arr.map { |sub_arr| sub_arr.map { |el| el.to_s}}
-    acc = []
-  end
+# the reduce method when called on an array of numeric values, passes the current vlaue along with an accumulator to the block. Within the block, the blocks return value is assigned to the accumulator and carried to the next iteration. After the method executes, it returns the accumulator.
 
-  counter = 0
+# the reduce method takes an optionally argument which specifies the accumulator starting value. 
 
-  loop do 
-    begin
-    return_value = yield(acc, arr[counter])
-    acc = return_value
-    counter += 1
-    break if counter == arr.size
-    rescue NoMethodError => e
-      print 'Incorrect implementation '
-      puts e.message
-      break
-    end
-  end
+# if a value is not passed then we default to using the first element in the collection and begin from the second element.
 
-  if acc.is_a?(Array)
-    acc.map { |el| el.match?(/[0-9]/) ? el.to_i : el } 
+
+def reduce(obj, default = nil)
+  if default.nil?
+    acc = obj.first
+    obj = obj[1..-1]
   else
-    acc
+    acc = default
   end
+
+  obj.each do |value|
+    acc = yield(acc, value) 
+
+  end
+  acc
 end
 
-array = [1, 2, 3, 4, 5]
+array = [1, 3, 4, 8, 5]
+nested_arr = [[1, 3], [4, 8], [5]]
+string = %w(joe is a hoe)
 
-p reduce(array) { |acc, num| acc + num }                    # => 15
-p reduce(array, 10) { |acc, num| acc + num }                # => 25
- reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+result = reduce(array) do |acc, value|
+  acc += value + 1
+end
 
-p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
-p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
+p result
+
+result = reduce(nested_arr, [1]) do |acc, sub_arr|
+  acc += sub_arr
+end
+p result
+
+result = reduce(string) do |acc, word|
+  acc += word
+end
+
+p result
+
+result = reduce(['a', 'b', 'c']) { |acc, value| acc += value }
+p result
+
+result = reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value }
+p result
