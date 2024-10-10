eaccba55b46257ee3dd1abda35a8b8f95e9b0b30
digitalphenom <digitalphenom0@gmail.com>
2024-10-10 13:57:35 -0700

    slight mod

--- RB_130/problem_sets/challenges/medium/diamond/10_diamond_two.rb
+++ RB_130/problem_sets/challenges/medium/diamond/10_diamond_two.rb
@@ -1,34 +1,31 @@
-
 class Diamond
   def self.make_diamond(n)
     top_alph = ("A"..n).to_a
     top_size = top_alph.size * 2
     bottom_alph = ("A"...n).to_a
     bottom_size = bottom_alph.size * 2 + 1
+    @@bottom = nil
     
-    #t = generate_top(top_alph, top_size)
-    b = generate_top(bottom_alph, bottom_size).reverse
-    #(t + b).join
+    diamond = generate_top(top_alph, top_size) +generate_top(bottom_alph, bottom_size).reverse
+    diamond.join("\n") + "\n"
   end
   
   class << self
     private
     
     def generate_top(sequence, size)
-      return ["A\n"] if sequence.size == 1
       @@count = 0
+      result = []
 
-      sequence.map do |char|
-        insert_chars(char).center(size) + "\n"
+      result = sequence.map do |letter|
+        insert_chars(letter).center(size)
       end
+      result
     end
 
-    def insert_chars(char)
-      if char == "A"
-        char
-      else
-        char + insert_white_space(char) + char
-      end
+    def insert_chars(letter)
+      return "A" if letter == "A" && @@bottom.nil?
+      letter + insert_white_space(letter) + letter
     end
 
     def insert_white_space(char)
@@ -43,15 +40,4 @@ class Diamond
   end
 end
 
-#Diamond.make_diamond("E")
-
-# space = 0
-#       Proc.new do |char|
-#         if char == "B"
-#           space += 1
-#           " " * space
-#         else
-#           space += 2
-#           " " * space
-#         end
-#       end
+p Diamond.make_diamond("B")
