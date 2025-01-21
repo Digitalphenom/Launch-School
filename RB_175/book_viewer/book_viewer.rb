31ce661a6d09ac980558d0e4ef02421420461b2c
digitalphenom <digitalphenom0@gmail.com>
2025-01-20 16:15:54 -0800

    added before filters and dynamic chapter nav

--- RB_175/book_viewer/book_viewer.rb
+++ RB_175/book_viewer/book_viewer.rb
@@ -2,15 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erb"
 
+before do
+  @toc_contents = File.read("data/toc.txt")
+end
+
 get "/" do
-  @contents = File.read("data/toc.txt")
+  @title = "The Adventures of Sherlock Holmes"
+
   erb :home
 end
 
-get "/chapters/1" do 
-  @title = "Chapter 1"
+get "/chapters/:number" do 
+  number = params[:number].to_i
+
+  chapter_name = @toc_contents.split(/\n/)[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
   @toc_contents = File.read('data/toc.txt')
-  @chapter_contents = File.read('data/chp1.txt')
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
