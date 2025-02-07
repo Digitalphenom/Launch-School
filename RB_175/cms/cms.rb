a321a41c243370a026cffb8c1960c4b27c19d8e5
digitalphenom <digitalphenom0@gmail.com>
2025-02-07 14:29:29 -0800

    added markdown rendering ability & test

--- RB_175/cms/cms.rb
+++ RB_175/cms/cms.rb
@@ -3,36 +3,53 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'erubi'
 require 'erubi/capture_block'
-#require 'minitest/autorun'
+require 'redcarpet'
+# require 'minitest/autorun'
 
-enable :sessions
-set :session_secret, SecureRandom.hex(32)
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
 
 helpers do
   def error_or_blank
-    session[:error] ? session[:error] : ""
+    session[:error] || ''
   end
 end
 
 get '/' do
-  root = File.expand_path('..', __FILE__)
+  root = File.expand_path(__dir__)
   @files = Dir.glob(root + '/files/*').map do |path|
     File.basename(path)
   end
   erb :index
 end
 
+def render_md(contents)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  contents.map { |line| markdown.render(line) }
+end
+
+def load_file_content(contents, doc)
+  file_type = doc.split('.').last
+
+  case file_type
+  when 'md'
+    headers['Content-Type'] = 'text/html'
+    render_md(contents)
+  when 'txt'
+    headers['Content-Type'] = 'text/plain'
+    contents
+  end
+end
+
 get '/:filename' do
-  headers 'Content-Type' => 'text/plain'
   doc = params[:filename]
   begin
-    File.readlines("files/#{doc}")
-  rescue StandardError
+    contents = File.readlines("files/#{doc}")
+    load_file_content(contents, doc)
+  rescue Errno::ENOENT
     session[:error] = "#{params[:filename]} does not exist"
     redirect '/'
   end
 end
-
-
-
-
--- RB_175/cms/test/cms_test.rb
+++ RB_175/cms/test/cms_test.rb
@@ -1,6 +1,6 @@
-ENV["RACK_ENV"] = "test"
+ENV['RACK_ENV'] = 'test'
 
-#Dir.chdir("..") unless Dir.pwd.end_with?("cms")
+# Dir.chdir("..") unless Dir.pwd.end_with?("cms")
 
 require_relative '../cms'
 
@@ -17,7 +17,7 @@ class CMSTest < Minitest::Test
   def test_index
     get '/'
     assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
     assert_equal true, last_response.body.match?(/history|changes|about/)
   end
 
@@ -30,15 +30,6 @@ class CMSTest < Minitest::Test
     assert_equal contents.join, last_response.body
   end
 
-  def test_about
-    get '/about.txt'
-
-    assert_equal 200, last_response.status
-    assert_equal 'text/plain', last_response['Content-Type']
-    contents = File.readlines('files/about.txt')
-    assert_equal contents.join, last_response.body
-  end
-
   def test_changes
     get '/changes.txt'
 
@@ -47,20 +38,28 @@ class CMSTest < Minitest::Test
     contents = File.readlines('files/changes.txt')
     assert_equal contents.join, last_response.body
   end
-  
+
   def test_dynamic_filename
-    get "/notafile.ext"
+    get '/notafile.ext'
     assert_equal 302, last_response.status
-    assert_raises StandardError do 
+    assert_raises StandardError do
       File.readlines("files/#{doc}")
     end
 
     get last_response['Location']
     assert_equal 200, last_response.status
     assert_includes last_response.body, 'notafile.ext does not exist'
-    
+
     get '/'
     refute_includes last_response.body, 'notafile.ext does not exist'
   end
 
+  def test_viewing_markdown_document
+    get '/about.md'
+    assert_equal 200, last_response.status
+    assert_equal 'text/html', last_response['Content-Type']
+
+    File.readlines('files/about.md')
+    assert_includes last_response.body, '<h1>Ruby is</h1>'
+  end
 end
