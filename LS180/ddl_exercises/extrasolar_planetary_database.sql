ef951a9a2094dfaa837e4d2adf0102e594732d06
Sedrick <digitalphenom0@gmail.com>
2025-05-01 16:31:56 -0700

    exploring decimal precisions complete

--- LS180/ddl_exercises/extrasolar_planetary_database.sql
+++ LS180/ddl_exercises/extrasolar_planetary_database.sql
@@ -28,6 +28,14 @@ ADD COLUMN star_id INT NOT NULL REFERENCES stars(id)
 ALTER TABLE stars
 ALTER COLUMN name TYPE VARCHAR(50);
 
-Updating the datatype to `VARCHAR(50)` does not raise an error  because the existing values meet the specified constraint of 50 chars. If we attempt to update to `VARCHAR(5)` an error is raised.
+-- Updating the datatype to `VARCHAR(50)` does not raise an error  because the existing values meet the specified constraint of 50 chars. If we attempt to update to `VARCHAR(5)` an error is raised.
 
 #◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
+
+4. 
+ALTER TABLE stars
+ALTER COLUMN distnace TYPE numeric
+
+-- updating the data type of INT to numeric when an existing column references a whole number does not automatically convert it into its decimal equivalent i.e. `4 => 4.0`, unless it is manually specified with `decimal(2, 1)`.
+
+-- The opposite is slightly different in that a decimal number `4.6` will auto round to 4. It's worth noting that this action is permanent so proceed with caution.
