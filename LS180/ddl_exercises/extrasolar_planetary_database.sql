a4a987725d7ad89480736566a7dc21d37644f3aa
Sedrick <digitalphenom0@gmail.com>
2025-05-02 14:26:24 -0700

    check values and further exploration complete

--- LS180/ddl_exercises/extrasolar_planetary_database.sql
+++ LS180/ddl_exercises/extrasolar_planetary_database.sql
@@ -39,3 +39,20 @@ ALTER COLUMN distnace TYPE numeric
 -- updating the data type of INT to numeric when an existing column references a whole number does not automatically convert it into its decimal equivalent i.e. `4 => 4.0`, unless it is manually specified with `decimal(2, 1)`.
 
 -- The opposite is slightly different in that a decimal number `4.6` will auto round to 4. It's worth noting that this action is permanent so proceed with caution.
+
+# ◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
+
+-- 5.  Add a constraint to ensure 'O', 'B', 'A', 'F', 'G', 'K', and 'M'are included.
+
+ALTER TABLE stars
+ADD CONSTRAINT check_type CHECK (spectral_type SIMILAR TO '%(O|B|A|F|G|K|M)%');
+
+#⋄≂≂▫≂≂▫≂≂▫≂▫≂▫≂≂▫≂≂▫≂⋄—◟ Further Exploration ◞—⋄≂▫≂≂▫≂≂▫≂≂▫≂▫≂≂▫≂≂▫≂≂≂⋄
+
+The further exploration problem arises from realising that the current data now requires a new set of constraints, not null and checking for invalid values i.e 'X'. However, how to add such constraints to an existing database that already contains "bad" values?
+
+Well, one option could be to make a backup table, delete all of the existing data then modify the schema so it adds a `NOT NULL` constraint with a `DEFAULT` value of 'N/A' when values fields are empty and 'INVALID' when an invalid char is encountered, then reinsert all of the data.
+
+This wouldnt work because the `spectral_type` data type is `CHAR(1)`, which only allows single char values. Another option could be to use "reserved" single chars to identify a column that contains a null value or invalid value. So rather than delete the existing data and then modifying the schema. The existing database could be cleaned for null and invalid chars, then the schema level changes would be applied to enforce the new constraints moving forward.
+
+A simpler, more immediate option would be to use `NOT VALID` with `CHECK` to temporarily avoid existing data that does not conform to the specified constraint. Then perform whatever cleaning on the existing data.
